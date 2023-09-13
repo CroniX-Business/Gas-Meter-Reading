e9229d46effc3edf246821b564e67e9998937991
CroniX-Business <petarruskan@gmail.com>
2023-09-13 15:25:46 +0200

    test_1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 from ultralytics import YOLO
 import easyocr
 import ssl
+import cv2
 ssl._create_default_https_context = ssl._create_unverified_context
 
 model = YOLO("runs/detect/yolov8/weights/best.pt")
@@ -17,6 +18,8 @@ def main():
     image = Image.open(uploaded_file)
     #st.image(image, caption="Originalna slika", use_column_width=True)
 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
     results = model.predict(source=image, save=False)
 
     for result in results:
@@ -24,18 +27,15 @@ def main():
 
         x1, y1, x2, y2 = map(int, coordinates[0])
 
-        #gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
         cropped_image = image[y1:y2, x1:x2]
+        upscaled_region = cv2.resize(cropped_image, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
         reader = easyocr.Reader(['en'])
         result = reader.readtext(cropped_image, detail = 0, allowlist='0123456789')
         #print(result)
 
+        st.image(upscaled_region, caption="Brojilo", use_column_width=True)
         st.write(result)
-
-#cv2.imshow('Cropped Image', cropped_image)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
 
 
 def style():
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 model = YOLO("runs/detect/yolov8/weights/best.pt")
 
-im2 = cv2.imread("test_1.jpeg")
+im2 = cv2.imread("datasets/Brojilo/test/images/00882400012550_0_jpg.rf.8c4ca7d251ff58546adabc1173770ae5.jpg")
 results = model.predict(source=im2, save=False)
 
 for result in results:
@@ -19,16 +19,18 @@ for result in results:
 for coord in coordinates:
     x1, y1, x2, y2 = map(int, coord)
     cropped_image = im2[y1:y2, x1:x2]
+    upscaled_region = cv2.resize(cropped_image, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
     
     reader = easyocr.Reader(['en'])
-    result = reader.readtext(cropped_image, detail=0, allowlist='0123456789')
+    result = reader.readtext(upscaled_region, detail=0, allowlist='0123456789')
     
     for res in result:
         print(res)
     
     #print("Text from cropped region:", result)
 
-cv2.imshow('Image with Bounding Boxes', im2)
+cv2.imshow('Image with Bounding Boxes', upscaled_region)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 

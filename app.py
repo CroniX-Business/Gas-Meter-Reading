cb31f1dc20d7bde9925d0ea209b940f6213b9d94
CroniX-Business <petarruskan@gmail.com>
2023-09-13 15:38:28 +0200

    test_2

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def main():
     for result in results:
         coordinates = result.boxes.xyxy
 
-        x1, y1, x2, y2 = map(int, coordinates[0])
+        x1, y1, x2, y2 = map(int, coordinates)
 
         cropped_image = image[y1:y2, x1:x2]
         upscaled_region = cv2.resize(cropped_image, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
